92e4718a443da4aa6180e6ccbfff9c613b0010e0
Realrishiiscool <2108120.rishivardhan@gmail.com>
2026-03-05 13:49:37 +0000

    added manual testing code and live server chgnes1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Index from "./pages/Index";
 import SelectTechnology from "./pages/SelectTechnology";
@@ -17,16 +17,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/select" element={<SelectTechnology />} />
           <Route path="/test" element={<TestPage />} />
-          <Route path="/results" element={<ResultsPage />} />
+          <Rout e path="/results" element={<ResultsPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
